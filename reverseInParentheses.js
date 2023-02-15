b9c5e10a27290b54816437ee39402bebc5b8a948
Woodshy jeanbaptiste <85188769+woodshyJean@users.noreply.github.com>
2023-02-15 09:43:07 -0500

    Update reverseInParentheses.js

--- reverseInParentheses.js
+++ reverseInParentheses.js
@@ -1,4 +1,4 @@
-Write a function that reverses characters in (possibly nested) parentheses in the input string.
+/*Write a function that reverses characters in (possibly nested) parentheses in the input string.
 
 Input strings will always be well-formed with matching ()s.
 
@@ -12,7 +12,7 @@ For inputString = "foo(bar)baz(blim)", the output should be
 solution(inputString) = "foorabbazmilb";
 For inputString = "foo(bar(baz))blim", the output should be
 solution(inputString) = "foobazrabblim".
-Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".
+Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".*/
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 function solution(inputString) {
